65237c19ce319f930378112bd9d4028a418833e5
Diego Kamiha <diego.kamiha@ifood.com.br>
2020-08-03 21:11:08 -0300

    Adicionando ringtone

--- prj3/AndroidStudioProject/HarryPotterSpells/app/src/main/java/com/example/harrypotterspells/SpellActivity.java
+++ prj3/AndroidStudioProject/HarryPotterSpells/app/src/main/java/com/example/harrypotterspells/SpellActivity.java
@@ -3,6 +3,7 @@ package com.example.harrypotterspells;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatImageView;
 
+import android.media.MediaPlayer;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
@@ -35,24 +36,43 @@ public class SpellActivity extends AppCompatActivity {
         TextView spellTitle = findViewById(R.id.spellScreenMessage);
         AppCompatImageView imageSpell = findViewById(R.id.spellImage);
 
+
         if(spell.equals("REVELIO")){
             spellText.setText("REVELIO");
             imageSpell.setImageResource(R.drawable.revelio);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.revelio);
+            mp.start();
         } else if(spell.equals("ALOHOMORA")) {
             spellText.setText("ALOHOMORA");
             imageSpell.setImageResource(R.drawable.alohomora);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.alohomora);
+            mp.start();
         } else if(spell.equals("ARRESTO_MOMENTUM")) {
             spellText.setText("ARRESTO MOMENTUM");
             imageSpell.setImageResource(R.drawable.arresto_momentum);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.arresto_momentum);
+            mp.start();
         } else if(spell.equals("INCENDIO")) {
             spellText.setText("INCENDIO");
             imageSpell.setImageResource(R.drawable.incendio);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.incendio);
+            mp.start();
         }else if(spell.equals("WINGARDIUM_LEVIOSA")) {
             spellText.setText("WINGARDIUM LEVIOSA");
             imageSpell.setImageResource(R.drawable.w_leviosa);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.wingardium_leviosa);
+            mp.start();
         }else if(spell.equals("FINITE_INCANTATEM")) {
             spellText.setText("FINITE INCANTATEM");
             imageSpell.setImageResource(R.drawable.finite_incantatem);
+
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.finite_incantatem);
+            mp.start();
         }else{
             spellTitle.setText("Well, I do not know what it is :(");
             spellText.setText("\"You are not a wizard, Harry\"");
