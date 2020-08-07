61c29fa37a26dcea2826fe809c3251130639def0
Anderson Nogueira Cotrim <ra163993@students.ic.unicamp.br>
2020-08-07 12:04:51 -0300

    add hardcoded server address

--- prj3/AndroidStudioProject/HarryPotterSpells/app/src/main/java/com/example/harrypotterspells/SetupActivity.java
+++ prj3/AndroidStudioProject/HarryPotterSpells/app/src/main/java/com/example/harrypotterspells/SetupActivity.java
@@ -11,25 +11,41 @@ import com.google.android.material.textfield.TextInputEditText;
 
 public class SetupActivity extends AppCompatActivity {
 
+    private String SERVER_ADDRESS = "";
+
+    private TextInputEditText serverAddressInputText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_setup);
 
         Button nextButton = findViewById(R.id.nextButton);
-        final TextInputEditText serverAddressInputText = findViewById(R.id.serverAddressInputText);
+        serverAddressInputText = findViewById(R.id.serverAddressInputText);
 
         nextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(SetupActivity.this, MainActivity.class);
-
-                Bundle b = new Bundle();
-                b.putString("serverAddress", serverAddressInputText.getText().toString());
-
-                intent.putExtras(b); //Put your id to your next Intent
-                startActivity(intent);
+                nextActivity();
             }
         });
+
+        if (!SERVER_ADDRESS.equals("")) {
+            serverAddressInputText.setText(SERVER_ADDRESS);
+            nextActivity();
+        }
+    }
+
+
+    private void nextActivity(){
+        Intent intent = new Intent(SetupActivity.this, MainActivity.class);
+
+        Bundle b = new Bundle();
+        b.putString("serverAddress", serverAddressInputText.getText().toString());
+
+        intent.putExtras(b); //Put your id to your next Intent
+        startActivity(intent);
     }
 }
